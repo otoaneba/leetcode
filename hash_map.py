ae48aa75737f57d15fde20c57f590f7704a7e38e
Naoto Abe <naotoabe1@gmail.com>
2026-01-08 21:12:55 +0100

    first commit

---
+++ hash_map.py
@@ -0,0 +1,10 @@
+"""
+LeetCode Hash Table / Dictionary problems
+Last updated: 2026-01 by Naoto
+"""
+
+from collections import defaultdict, Counter
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+  return 0
---
+++ run_tests.py
@@ -0,0 +1,13 @@
+from hash_map import twoSum
+# from two_pointers import ...
+# from dp import ...
+
+def run():
+    # Two Sum
+    print(twoSum([2,7,11,15], 9))          # [0,1]
+    print(twoSum([3,2,4], 6))              # [1,2]
+
+    print("All tests finished ✓")
+
+if __name__ == "__main__":
+    run()
