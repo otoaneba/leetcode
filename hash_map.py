a9b55c7e6a97255e9e9c0880993140696e2a626e
Naoto Abe <naotoabe1@gmail.com>
2026-01-09 21:27:10 +0100

    finish singleNumber

--- hash_map.py
+++ hash_map.py
@@ -8,3 +8,25 @@ from typing import List
 
 def twoSum(nums: List[int], target: int) -> List[int]:
   return 0
+
+def singleNumber(nums: List[int]) -> int:
+  for x in nums:
+    count = 0
+    for y in nums:
+      if y == x:
+        count+=1
+    if count == 1:
+      return x
+
+# [2,2,1]
+def singleNumberHashMap(nums: List[int]) -> int:
+  seen = set()
+  for x in nums:
+    if x in seen:
+      seen.remove(x)
+    else:
+      seen.add(x)
+  
+  return seen.pop()
+  
+  
--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,5 @@
 from hash_map import twoSum
+from hash_map import singleNumberHashMap
 # from two_pointers import ...
 # from dp import ...
 
@@ -8,6 +9,9 @@ def run():
     print(twoSum([3,2,4], 6))              # [1,2]
 
     print("All tests finished ✓")
+
+    print(singleNumberHashMap([2,2,1]))
+    print(singleNumberHashMap([4,2,1,2,1]))
 
 if __name__ == "__main__":
     run()
