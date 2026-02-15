0723e81f808be61aa6aeae39379efb6ad6390ed6
Naoto Abe <naotoabe1@gmail.com>
2026-02-15 20:39:54 +0100

    add heap sort

--- arrays.py
+++ arrays.py
@@ -244,3 +244,123 @@ class MyHashSet:
   def contains(self, key: int) -> bool:
       return self.buckets[key] == True
 
+# Design a HashMap without using any built-in hash table libraries.
+
+# Implement the MyHashMap class:
+
+# MyHashMap() initializes the object with an empty map.
+# void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+# int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+# void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+# Example 1:
+
+# Input: ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+# [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+
+# Output: [null, null, null, 1, -1, null, 1, null, -1]
+# Explanation:
+# MyHashMap myHashMap = new MyHashMap();
+# myHashMap.put(1, 1); // The map is now [[1,1]]
+# myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+# myHashMap.get(1); // return 1, The map is now [[1,1], [2,2]]
+# myHashMap.get(3); // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+# myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+# myHashMap.get(2); // return 1, The map is now [[1,1], [2,1]]
+# myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+# myHashMap.get(2); // return -1 (i.e., not found), The map is now [[1,1]]
+
+# Constraints:
+
+# 0 <= key, value <= 1,000,000
+# At most 10,000 calls will be made to put, get, and remove.
+
+class MyHashMap:
+
+    def __init__(self):
+        self.buckets = [-1] * 1_000_001
+
+    def put(self, key: int, value: int) -> None:
+        self.buckets[key] = value
+
+    def get(self, key: int) -> int:
+        return self.buckets[key]
+
+    def remove(self, key: int) -> None:
+        self.buckets[key] = -1
+
+    # def __init__(self):
+    #     self.size = 10_000
+    #     self.buckets = [[] for _ in range(self.size)]
+
+    # def put(self, key: int, value: int) -> None:
+    #     hash_value = key % self.size
+    #     for pair in self.buckets[hash_value]:
+    #         if pair[0] == key:
+    #             pair[1] = value
+    #             return    
+    #     self.buckets[hash_value].append([key, value])
+        
+
+    # def get(self, key: int) -> int:
+    #     hash_value = key % self.size
+    #     for pair in self.buckets[hash_value]:
+    #         if pair[0] == key:
+    #             return pair[1]
+    #     return -1
+
+    # def remove(self, key: int) -> None:
+    #     hash_value = key % self.size
+
+    #     for pair in self.buckets[hash_value]:
+    #         if pair[0] == key:
+    #             self.buckets[hash_value].remove(pair)
+
+# You are given an array of integers nums, sort the array in ascending order and return it.
+
+# You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+# Example 1:
+
+# Input: nums = [10,9,1,1,1,2,3,1]
+
+# Output: [1,1,1,1,2,3,9,10]
+# Example 2:
+
+# Input: nums = [5,10,2,1,3]
+
+# Output: [1,2,3,5,10]
+# Constraints:
+
+# 1 <= nums.length <= 50,000.
+# -50,000 <= nums[i] <= 50,000
+class Heap_Sort:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        
+        # phase 1: build max heap
+        for index in range((n - 2) // 2, -1, -1):
+            self.heapify(nums, index, n)
+
+        # phase 2: repeat until sorted
+        for end in range(n - 1, 0, -1):
+            nums[0], nums[end] = nums[end], nums[0]
+
+            self.heapify(nums, 0, end)
+        return nums
+
+    def heapify(self, nums: List[int], i: int, heap_size: int) -> None:
+        while True:
+            left = 2*i + 1
+            right = 2*i + 2
+            largest = i
+
+            if left < heap_size and nums[left] > nums[largest]:
+                largest = left
+            if right < heap_size and nums[right] > nums[largest]:   
+                largest = right
+            
+            if largest == i:
+                return 
+            
+            nums[i], nums[largest] = nums[largest], nums[i]
+            i = largest
