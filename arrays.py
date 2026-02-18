d812ca1f0eff86bede5917c8a1d79f11356e6e34
Naoto Abe <naotoabe1@gmail.com>
2026-02-18 21:54:30 +0100

    solve encode decode

--- arrays.py
+++ arrays.py
@@ -437,3 +437,119 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         res.append(heapq.heappop(heap)[1])
     return res
     
+# Encode and Decode Strings
+# Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+# Machine 1 (sender) has the function:
+
+# string encode(vector<string> strs) {
+#     // ... your code
+#     return encoded_string;
+# }
+# Machine 2 (receiver) has the function:
+
+# vector<string> decode(string s) {
+#     //... your code
+#     return strs;
+# }
+# So Machine 1 does:
+
+# string encoded_string = encode(strs);
+# and Machine 2 does:
+
+# vector<string> strs2 = decode(encoded_string);
+# strs2 in Machine 2 should be the same as strs in Machine 1.
+
+# Implement the encode and decode methods.
+
+# Example 1:
+
+# Input: dummy_input = ["Hello","World"]
+
+# Output: ["Hello","World"]
+
+# Explanation:
+# Machine 1:
+# Codec encoder = new Codec();
+# String msg = encoder.encode(strs);
+# Machine 1 ---msg---> Machine 2
+
+# Machine 2:
+# Codec decoder = new Codec();
+# String[] strs = decoder.decode(msg);
+# Example 2:
+
+# Input: dummy_input = [""]
+
+# Output: [""]
+
+# Constraints:
+
+# 0 <= strs.length < 100
+# 0 <= strs[i].length < 200
+# strs[i] contains any possible characters out of 256 valid ASCII characters.
+
+# Follow up: Could you write a generalized algorithm to work on any possible set of characters?
+
+class EncodeDecodeSolution1:
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        sizes, res = [], ""
+        for s in strs:
+            sizes.append(len(s))
+        for sz in sizes:
+            res += str(sz)
+            res += ','
+        res += '#'
+        for s in strs:
+            res += s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return []
+        sizes, res, i = [], [], 0
+        while s[i] != '#':
+            cur = ""
+            while s[i] != ',':
+                cur += s[i]
+                i += 1
+            sizes.append(int(cur))
+            i += 1
+        i += 1
+        for sz in sizes:
+            res.append(s[i:i + sz])
+            i += sz
+        return res
+
+class EncodeDecodeSolution2:
+
+    def encode(self, strs: List[str]) -> str:
+        # for each string, take the length
+        # append the length with a delimiter, &
+        # return the string
+        res = ""
+        for s in strs:    
+            res += str(len(s)) + "&" + s
+        
+        print(res)
+        return res
+        
+    def decode(self, s: str) -> List[str]:
+        res = []
+
+        currIndex = 0
+
+        while currIndex < len(s):
+            j = currIndex
+            while s[j] != "&":
+                j += 1
+            length = int(s[currIndex:j])
+            currIndex = j + 1
+            j = currIndex + length
+            res.append(s[currIndex:j])
+            currIndex = j
+         
+        return res
+
