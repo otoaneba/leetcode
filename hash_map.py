ffa35591d225e4e81f39f1c7aca039c7bb3442d5
Naoto Abe <naotoabe1@gmail.com>
2026-02-26 15:55:56 +0100

    add longest palindrome

--- hash_map.py
+++ hash_map.py
@@ -162,3 +162,20 @@ def missingNumber(num: List[int]) -> int:
     listSum += value
   
   return sum(range(len(num)+1)) - listSum
+
+def longestPalindrome(self, s: str) -> str:
+  res = ""
+  for i in range(len(s)):
+      tmp = self.helper(s, i, i)
+      if len(tmp) > len(res):
+          res = tmp
+          
+      tmp = self.helper(s, i, i+1)
+      if len(tmp) > len(res):
+          res = tmp
+  return res
+
+def helper(self, s, l, r):
+  while l >= 0 and r < len(s) and s[l] == s[r]:
+      l -= 1; r += 1
+  return s[l+1:r]
